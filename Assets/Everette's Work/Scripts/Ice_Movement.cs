2d24e07f0d7cf48224e5835914e19f548000b486
Digx7 <37136301+Digx7@users.noreply.github.com>
2021-03-23 20:54:22 -0700

    Added basic ICE form movement and breakable walls

---
+++ Assets/Everette's Work/Scripts/Ice_Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ice_Movement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float speed;
+    [SerializeField] private Vector2 directionToMove;
+
+    public void FixedUpdate(){
+      Move(directionToMove);
+    }
+
+    public void setMoveDirection(Vector2 _directionToMove){
+      directionToMove = _directionToMove;
+    }
+
+    public void Move(Vector2 directionToMove) {
+      Debug.Log("Attempting to move ICE");
+      rb.AddForce(directionToMove * speed);
+    }
+}
