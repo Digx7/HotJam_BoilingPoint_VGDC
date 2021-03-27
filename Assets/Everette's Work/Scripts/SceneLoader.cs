ae292e98eb1fc103295f3b72e02355cd13d95275
Digx7 <37136301+Digx7@users.noreply.github.com>
2021-03-26 23:45:07 -0700

    Finished Main Menu

---
+++ Assets/Everette's Work/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string newSceneName;
+
+    public void _LoadScene(){
+      SceneManager.LoadScene(newSceneName);
+    }
+
+    public void QuitApp(){
+      Application.Quit();
+    }
+}
