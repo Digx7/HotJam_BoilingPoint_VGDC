111a9f9d09ece27d514aa5a8ee1debe85476ef44
Bryan Ambriz <beambriz2017@gmail.com>
2021-03-26 22:57:30 -0700

    level1 done

--- Assets/Everette's Work/Scripts/Ice_BreakableWall.cs
+++ Assets/Everette's Work/Scripts/Ice_BreakableWall.cs
@@ -7,13 +7,21 @@ public class Ice_BreakableWall : MonoBehaviour
     [SerializeField] private string tagToLookFor;
     [SerializeField] private float impactTreshHold = 1.0f;
     [SerializeField] private GameObject breakingParticles;
-
+    [SerializeField] private bool xAxis;
     public void OnCollisionEnter2D(Collision2D col){
       Vector2 impactVelocity = col.relativeVelocity;
       //Debug.Log("Impact Velocity = " + impactVelocity.x);
 
-      if(col.gameObject.tag == tagToLookFor && impactVelocity.x > impactTreshHold) WallBreak();
-      //else Debug.Log("Wall is good");
+        if(xAxis)
+        {
+            if (col.gameObject.tag == tagToLookFor && impactVelocity.x > impactTreshHold) WallBreak();
+
+        }
+        else
+        {
+            if (col.gameObject.tag == tagToLookFor && impactVelocity.y > impactTreshHold) WallBreak();
+        }
+        //else Debug.Log("Wall is good");
     }
 
     private void WallBreak(){
