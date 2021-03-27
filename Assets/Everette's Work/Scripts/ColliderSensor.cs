275435e75e1880c872767a67b383ad6c846b7164
Digx7 <37136301+Digx7@users.noreply.github.com>
2021-03-27 00:04:00 -0700

    Build we submitted

---
+++ Assets/Everette's Work/Scripts/ColliderSensor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ColliderSensor : MonoBehaviour
+{
+    public UnityEvent ouch;
+
+    public void OnCollisionEnter2d(Collider2D col){
+      ouch.Invoke();
+    }
+}
